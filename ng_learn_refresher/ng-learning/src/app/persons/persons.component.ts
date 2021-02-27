30e9955e9222cb4b47bcf89f938df046bda54f4b
ShvetkumarPatel <patelshvet@gmail.com>
2021-02-27 13:04:14 -0500

    creating first own component

---
+++ ng_learn_refresher/ng-learning/src/app/persons/persons.component.ts
@@ -0,0 +1,11 @@
+
+import { templateJitUrl } from '@angular/compiler';
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html'
+})
+export class PersonsComponent{
+
+}
